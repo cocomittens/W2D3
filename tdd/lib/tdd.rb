e52756e9ab45fe12ed852d0a6b5a685f19663d9d
ioqren <corriestoddard@gmail.com>
2019-01-17 06:40:00 -0800

    Added Towers of Hanoi class and specs

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -5,10 +5,10 @@ class Array
         result.keys
     end
 
-    def pair_sum
+    def two_sum
     result = []
     self.each_with_index do |el, i|
-      pair = - el
+      pair = -el
       self.each_with_index do |other_el, j|
         if other_el == pair && !result.include?([j, i]) && i!=j
           result << [i, j]
@@ -16,5 +16,119 @@ class Array
       end
     end
     result
+  end
+
+  def my_transpose
+    result = Array.new(self.length){[]}
+    self.each_index do |idx|
+      self.each do |arr|
+        result[idx] << arr[idx]
+      end
+    end
+    result
+  end
+end
+
+class TowersOfHanoi
+  attr_accessor :towers
+
+  def initialize
+    @towers = [[3, 2, 1], [], []]
+  end
+
+  def play
+    while !won?
+      puts "What pile to move from? (1-3)"
+      from = gets.chomp.to_i
+      puts "What pile to move to? (1-3)"
+      to = gets.chomp.to_i
+      self.move(from-1, to-1)
+      self.render
+    end
+  end
+
+  def render
+    puts "1: " + @towers[0].to_s
+    puts "2: " + @towers[1].to_s
+    puts "3: " + @towers[2].to_s
+  end
+
+  def won?
+    if @towers[0].empty? && (@towers[1].empty? || @towers[2].empty?)
+      puts ("You won!")
+      return true
+    else
+      return false
+    end
+  end
+
+  def valid_move?(from_tower, to_tower)
+    if @towers[from_tower].empty?
+      return false
+    elsif @towers[to_tower].empty?
+      return true
+    elsif @towers[to_tower].last < @towers[from_tower].last
+      return false
+    else
+      return true
+    end
+  end
+
+  def move(from_tower, to_tower)
+    if valid_move?(from_tower, to_tower)
+      @towers[to_tower] << @towers[from_tower].pop()
+    end
+  end
+end
+
+class TowersOfHanoi
+  attr_accessor :towers
+
+  def initialize
+    @towers = [[3, 2, 1], [], []]
+  end
+
+  def play
+    while !won?
+      puts "What pile to move from? (1-3)"
+      from = gets.chomp.to_i
+      puts "What pile to move to? (1-3)"
+      to = gets.chomp.to_i
+      self.move(from-1, to-1)
+      self.render
+    end
+    puts ("You won!")
+  end
+
+  def render
+    puts "1: " + @towers[0].to_s
+    puts "2: " + @towers[1].to_s
+    puts "3: " + @towers[2].to_s
+  end
+
+  def won?
+    if @towers[0].empty? && (@towers[1].empty? || @towers[2].empty?)
+      return true
+    else
+      return false
+    end
+  end
+
+  def valid_move?(from_tower, to_tower)
+    if @towers[from_tower].empty?
+      return false
+    elsif @towers[to_tower].empty?
+        return true
+    elsif @towers[to_tower].last < @towers[from_tower].last
+        return false
+    else
+      return true
+    end
+  end
+
+  def move(from_tower, to_tower)
+    if valid_move?(from_tower, to_tower)
+      @towers[to_tower] << @towers[from_tower].pop
+    end
   end
 end
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -10,16 +10,72 @@ describe "#my_uniq" do
   end
 end
 
-describe "#pair_sum" do
+describe "#two_sum" do
   it "returns the index of pairs that sum to zero" do
-    expect([3, 4, -4, 6].pair_sum).to eq([[1, 2]])
+    expect([3, 4, -4, 6].two_sum).to eq([[1, 2]])
   end
 
   it "can return multiple pairs" do
-    expect([3, -3, 5, -5].pair_sum).to eq([[0, 1], [2, 3]])
+    expect([3, -3, 5, -5].two_sum).to eq([[0, 1], [2, 3]])
   end
 
   it "sorts by original index of first elements, then second" do
-    expect([3, 5, -3, 3, -5, -5].pair_sum).to eq([[0, 2], [1, 4], [1, 5], [2, 3]])
+    expect([3, 5, -3, 3, -5, -5].two_sum).to eq([[0, 2], [1, 4], [1, 5], [2, 3]])
+  end
+end
+
+describe "#my_transpose" do
+    let(:rows){
+    [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+    }
+    let(:cols){
+    [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+    ]
+    }
+    it "works with a 2x2 matrix" do
+        a = [[1, 5], [7, 2]]
+        b = [[1, 7], [5, 2]]
+        expect(a.my_transpose).to eq(b)
+    end
+
+    it "converts a 3x3 matrix from rows to columns" do
+        expect(rows.my_transpose).to eq(cols)
+    end
+
+    it "converts a 3x3 matrix from columns to rows" do
+        expect(cols.my_transpose).to eq(rows)
+    end
+
+    
+end
+
+describe TowersOfHanoi do
+  let(:game) { TowersOfHanoi.new }
+  let(:towers) { game.towers }
+  describe "#move" do
+
+
+    before(:each) do
+      game.move(0, 1)
+    end
+
+    it "pops the disc off of from_tower" do
+      expect(towers[0]).to eq([3, 2])
+    end
+
+    it "pushes the disc onto to_tower" do
+      expect(towers[1]).to eq([1])
+    end
+
+    it "doesn't change uninvolved towers" do
+      expect(towers[2]).to be_empty
+    end
   end
 end
